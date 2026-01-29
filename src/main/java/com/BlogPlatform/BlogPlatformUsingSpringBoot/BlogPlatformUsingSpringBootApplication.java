fad73d67fb52ab99c1ffa324af7f410ee84aed94
H5misi <hazm.sayed.m@gmail.com>
2026-01-29 16:54:18 +0200

    Initializing the project using Spring Initializr

---
+++ src/main/java/com/BlogPlatform/BlogPlatformUsingSpringBoot/BlogPlatformUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.BlogPlatform.BlogPlatformUsingSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPlatformUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPlatformUsingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BlogPlatform/BlogPlatformUsingSpringBoot/BlogPlatformUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlogPlatform.BlogPlatformUsingSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
